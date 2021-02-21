44b60f195891a6ec838807b8f0e6f6e6f7c876c9
dommeudec <72738163+dommeudec@users.noreply.github.com>
2021-02-21 14:32:58 +0000

    Background Image Added

--- Fisconnect/src/App.js
+++ Fisconnect/src/App.js
@@ -1,36 +1,40 @@
 import './App.css';
+
 import'bootstrap/dist/css/bootstrap.css';
-import { Container, Row, Col, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Container, Row, Col, Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
 
 function App() {
   return (
-    
     <div className="App">
+      
       <div>
-      <Navbar bg="primary" variant="dark">
-        <Navbar.Brand href="#home">Fisconnect</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#freelancers">Freelancers</Nav.Link>
-          <Nav.Link href="#about_us">About Us</Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-light">Search</Button>
-        </Form>
-      </Navbar>
+        <Navbar bg="dark" variant="dark" className="navbar">
+          <Navbar.Brand href="#home">Fisconnect</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#freelancers">Freelancers</Nav.Link>
+            <Nav.Link href="#about_us">About Us</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-light">Search</Button>
+          </Form>
+        </Navbar>
       </div>
 
-      <div>
-      <h1>
-        FISCONNECT!
-      </h1>
-      <Container fluid>
-        <Row>
-          <Col>1 of 1</Col>
-        </Row>
-      </Container>
-      </div>
+        <Container fluid className="homepageWelcome" style={{ backgroundImage: "url(/Homepage.png)", height: '750px' }}>
+          <Row>
+            <Col>
+              <h1>FÍSCONNECT</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+            <h3>WE CONNECT BUSINESSES TO FREELANCE VIDEOGRAPHERS & PHOTOGRAPHERS</h3>
+            </Col>
+          </Row>
+        </Container>
+
     </div>
 
   );
