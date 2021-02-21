a3f69fc59135d11f3f5e0edc8a4c619e04766834
dommeudec <72738163+dommeudec@users.noreply.github.com>
2021-02-21 13:19:18 +0000

    Basic Navbar added
    Simple navigation bar added, tweeking needed for style and use

--- Fisconnect/src/App.js
+++ Fisconnect/src/App.js
@@ -1,13 +1,38 @@
 import './App.css';
+import'bootstrap/dist/css/bootstrap.css';
+import { Container, Row, Col, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 
 function App() {
   return (
+    
     <div className="App">
+      <div>
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">Fisconnect</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#freelancers">Freelancers</Nav.Link>
+          <Nav.Link href="#about_us">About Us</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-light">Search</Button>
+        </Form>
+      </Navbar>
+      </div>
+
+      <div>
       <h1>
         FISCONNECT!
       </h1>
-      
+      <Container fluid>
+        <Row>
+          <Col>1 of 1</Col>
+        </Row>
+      </Container>
+      </div>
     </div>
+
   );
 }
 
