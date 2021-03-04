0c265efbfd882534287b459f019da0ec3ca2a659
FilipKowalski <kowalskf@tcd.ie>
2021-03-04 15:14:10 +0000

    Portfolio page can use id paramater
    the portfolio page now extracts the specific id of the freelancer

--- Fisconnect/src/Routes.js
+++ Fisconnect/src/Routes.js
@@ -16,7 +16,7 @@ export default function Routes() {
         <Route exact path="/freelancers">
           <Freelancers />
         </Route>
-        <Route exact path ="/freelancers/portfolio/:id">
+        <Route exact path ="/freelancers/portfolio/:id" render={(props) => <Portfolio id={this.match.params.id} /> }>
           <Portfolio />     
         </Route>
     </Switch>
--- Fisconnect/src/containers/Portfolio.js
+++ Fisconnect/src/containers/Portfolio.js
@@ -1,16 +1,17 @@
 //for now i just copied the home page for testing
 
 import './Home.css';
-
 import'bootstrap/dist/css/bootstrap.css';
 import { Container, Row, Col, Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
-
+import React, { Component } from 'react';
+import { useParams } from 'react-router';
 
 function App() {
+    const { id } = useParams();
   return (
     <div className="Home">
       <Container fluid className="homepageWelcome" style={{ backgroundImage: "url(/images/Homepage2.jpg)"}} id="background_img">
-        <h1>TEST</h1>
+        <h1>{id}</h1>
         <h3>TEST</h3>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
