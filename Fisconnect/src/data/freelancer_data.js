a0a224c9829a78d5fbf3a567c2569e6d8596c930
alicedoherty <66687032+alicedoherty@users.noreply.github.com>
2021-03-04 14:26:59 +0000

    Added currency symbol

--- Fisconnect/src/data/freelancer_data.js
+++ Fisconnect/src/data/freelancer_data.js
@@ -4,7 +4,7 @@ const freelancer_info = [
         name:"John Doe",
         job_title:"Videographer",
         sector:"Retail, Events",
-        full_day_rate: 100,
+        full_day_rate: "€100",
         experience_years: 5,
         thumbnail: "./images/thumb1.jpg"
     },
@@ -13,7 +13,7 @@ const freelancer_info = [
         name:"Michael Wood",
         job_title:"Videographer",
         sector:"Hospitality, Events",
-        full_day_rate: 200,
+        full_day_rate: "€200",
         experience_years: 2,
         thumbnail: "./images/thumb2.jpg"
     },
@@ -22,7 +22,7 @@ const freelancer_info = [
         name:"Ben Murphy",
         job_title:"Photgrapher",
         sector:"Retail, Events",
-        full_day_rate: 160,
+        full_day_rate: "€160",
         experience_years: 1,
         thumbnail: "./images/thumb3.jpg"
     },
@@ -31,7 +31,7 @@ const freelancer_info = [
         name:"Jane Doe",
         job_title:"Videographer",
         sector:"Corporate",
-        full_day_rate: 40,
+        full_day_rate: "€40",
         experience_years: 7,
         thumbnail: "./images/thumb4.jpg"
     }
