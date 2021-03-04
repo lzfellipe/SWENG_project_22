2febab9af39e60a9bbf1267bcc88157a7d2e7e64
alicedoherty <66687032+alicedoherty@users.noreply.github.com>
2021-03-04 12:00:29 +0000

    Start of browse freelancers

--- Fisconnect/src/containers/Freelancers.js
+++ Fisconnect/src/containers/Freelancers.js
@@ -1,13 +1,42 @@
 import React from "react";
+import freelancer_info from "../data/freelancer_data";
+import './Freelancers.css';
 
-function Page2() {
+// function FreelancerBrowse() {
 
-     return (
-       <div>
-         <p>This is the freelancer page.</p>
-       </div>
-     );
+//      return (
+//        <div>
+//          <p>This is the freelancer page.</p>
+//        </div>
+//      );
 
- }
+//  }
 
- export default Page2;
+const FreelancerBrowse = () => {
+  console.log(freelancer_info);
+  const freelancerList = freelancer_info.map((freelancer) =>
+    <div className="freelancer_card" key={freelancer.id}>
+      <div className="card_thumbnail">
+        <img src={freelancer.thumbnail} />
+      </div>
+      <div className="card_header">
+        <h2>{freelancer.name}</h2>
+        <h3>{freelancer.job_title}</h3>
+        <p>{freelancer.sector}</p>
+        <p className="price">{freelancer.full_day_rate}</p>
+        <div className="btn">See more</div>
+      </div>
+
+    </div>
+  );
+
+  return(
+    <div className="main_content">
+      <h1>Browse Freelancers</h1>
+      {freelancerList}
+    </div>
+  )
+
+}
+
+export default FreelancerBrowse;
---
+++ Fisconnect/src/data/freelancer_data.js
@@ -0,0 +1,39 @@
+const freelancer_info = [
+    {
+        id: 1,
+        name:"John Doe",
+        job_title:"Videographer",
+        sector:"Retail, Events",
+        full_day_rate: 100,
+        experience_years: 5,
+        thumbnail: "./images/thumb1.png"
+    },
+    {
+        id: 2,
+        name:"Michael Wood",
+        job_title:"Videographer",
+        sector:"Hospitality, Events",
+        full_day_rate: 200,
+        experience_years: 2,
+        thumbnail: "./images/thumb1.png"
+    },
+    {
+        id: 3,
+        name:"Ben Murphy",
+        job_title:"Photgrapher",
+        sector:"Retail, Events",
+        full_day_rate: 160,
+        experience_years: 1,
+        thumbnail: "./images/thumb1.png"
+    },
+    {
+        id: 4,
+        name:"Jane Doe",
+        job_title:"Videographer",
+        sector:"Corporate",
+        full_day_rate: 40,
+        experience_years: 7,
+        thumbnail: "./images/thumb1.png"
+    }
+]
+export default freelancer_info;
