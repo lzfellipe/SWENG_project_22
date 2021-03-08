d0739e7b5e57dbce4fa32bcc7f0507d19653a24e
FilipKowalski <kowalskf@tcd.ie>
2021-03-08 19:11:17 +0000

    added carousel (not pretty WIP)
    added carousel for freelancer photos

--- Fisconnect/src/containers/Portfolio.js
+++ Fisconnect/src/containers/Portfolio.js
@@ -2,22 +2,34 @@
 
 import './Portfolio.css';
 import'bootstrap/dist/css/bootstrap.css';
-import { Container, Row, Col, Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
+import { Container, Carousel, Row, Col, Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
 import React, { Component } from 'react';
 import { useParams } from 'react-router';
 import freelancer_info from "../data/freelancer_data";
+import Image from 'react-bootstrap/Image';
+
 function Portfolio(props) {
     const {id} = useParams();
     const freelancer = freelancer_info[id];
 
   return (
     <div className="Portfolio">
-      <Container fluid className="Portfolio">
-        <h1>{freelancer.name}, {freelancer.job_title}</h1>
-        <h1>Full Day Rate Of {freelancer.full_day_rate}</h1>
-        <h1>{freelancer.experience_years} Years Of Experience</h1>
-        <h2>Specialises in {freelancer.sector}</h2>
+      <Container>
+        <img src="../../images/thumb1.jpg" />
+        <Carousel>
+        {freelancer.photos.map(photo => (
+          <Carousel.Item>
+            <img src={photo}/>
+            <Carousel.Caption>
+             <h3>test portfolio Carousel</h3>
+             <h1>it just works.</h1>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+        </Carousel>
       </Container>
+    
+     
     </div>
 
   );
--- Fisconnect/src/data/freelancer_data.js
+++ Fisconnect/src/data/freelancer_data.js
@@ -6,7 +6,8 @@ const freelancer_info = [
         sector:"Retail, Events",
         full_day_rate: "€100",
         experience_years: 5,
-        thumbnail: "./images/thumb1.jpg"
+        thumbnail: "../../images/thumb1.jpg",
+        photos: ["../../images/thumb1.jpg", "../../images/thumb2.jpg"]
     },
     {
         id: 1,
@@ -15,7 +16,8 @@ const freelancer_info = [
         sector:"Hospitality, Events",
         full_day_rate: "€200",
         experience_years: 2,
-        thumbnail: "./images/thumb2.jpg"
+        thumbnail: "../../images/thumb2.jpg",
+        photos: ["../../images/thumb2.jpg"]
     },
     {
         id: 2,
@@ -24,7 +26,8 @@ const freelancer_info = [
         sector:"Retail, Events",
         full_day_rate: "€160",
         experience_years: 1,
-        thumbnail: "./images/thumb3.jpg"
+        thumbnail: "../../images/thumb3.jpg",
+        photos: ["../../images/thumb3.jpg"]
     },
     {
         id: 3,
@@ -33,16 +36,18 @@ const freelancer_info = [
         sector:"Corporate",
         full_day_rate: "€40",
         experience_years: 7,
-        thumbnail: "./images/thumb4.jpg"
+        thumbnail: "../../images/thumb4.jpg",
+        photos: ["../../images/thumb4.jpg"]
     },
     {
-        id: 7,
+        id: 4,
         name:"Hi",
         job_title:"Videoapher",
         sector:"Corpore",
         full_day_rate: "€0",
         experience_years: 7,
-        thumbnail: "./images/Homepage2.jpg"
+        thumbnail: "../../images/Homepage2.jpg",
+        photos: ["../../images/Homepage2.jpg"]
     },
 
 ]
