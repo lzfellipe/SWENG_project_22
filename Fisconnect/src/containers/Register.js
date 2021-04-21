0ec8604c4aaa9c5c9a6f71ab817ba830287315e5
dommeudec <72738163+dommeudec@users.noreply.github.com>
2021-04-21 17:41:32 +0100

    Email now sends when user submits register
    Integrated email.js to submit an email to the business email when a user submits a register form.

--- Fisconnect/src/containers/Register.js
+++ Fisconnect/src/containers/Register.js
@@ -3,6 +3,7 @@ import './Register.css';
 import { useState } from 'react';
 import NavigationBar from '../containers/NavigationBar.js'
 import Footer from '../containers/Footer.js'
+import * as emailjs from "emailjs-com";
 
 function Register() {
     const [validated, setValidated] = useState(false);
@@ -14,7 +15,44 @@ function Register() {
             event.stopPropagation();
         }
         setValidated(true);
+        sendEmail();
     };
+
+    /*  For emails to be sent, an account must be created at emailjs.com. You must create an email service which is linked to your bubble vision email.
+        Within the sendform() function there are 4 paramenters. You must fill the parameters with your own details:
+        Parameter 1: Service ID
+        Parameter 2: Email template to be used
+        Parameter 3: DO NOT CHANGE
+        Parameter 4: User ID
+        
+        Sample Email template:
+
+        You got a register request from {{fname}} {{lname}}.
+
+        Personal details are as follows:
+
+        Email: {{email}}
+
+        Phone: {{phone}}
+
+        Start Date: {{startDate}}
+
+        Expertise: {{expertise}}
+        
+        */
+
+    function sendEmail(e){
+        e.preventDefault();
+
+
+    emailjs.sendForm('gmail', 'register_form_email', e.target, 'user_vD1r0QZwdo7HZhvbbV60H')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset();
+    }
 
     return (
         <div className="blue-background yellow-font">
@@ -27,33 +65,31 @@ function Register() {
             <h5>2. Seamless invoice management systems</h5>
             <h5>3. Steady cashflows<br></br><br></br></h5>
 
-
-
-            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form noValidate validated={validated} onSubmit={handleSubmit, sendEmail}>
                 <Form.Row>
                     <Form.Group as={Col} controlId="formGridEmail">
                         <Form.Label>Name</Form.Label>
-                        <Form.Control required type="text" placeholder="John" />
+                        <Form.Control required type="text" placeholder="John" name="fname"/>
                         <Form.Control.Feedback type="invalid">Please enter your name!</Form.Control.Feedback>
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridPassword">
                         <Form.Label>Last Name</Form.Label>
-                        <Form.Control required type="text" placeholder="Smith" />
+                        <Form.Control required type="text" placeholder="Smith" name="lname" />
                         <Form.Control.Feedback type="invalid">Please enter your last name!</Form.Control.Feedback>
                     </Form.Group>
                 </Form.Row>
                 <Form.Row>
                     <Form.Group as={Col} controlId="formGridEmail">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control required type="email" placeholder="Enter email" />
+                        <Form.Control required type="email" placeholder="Enter email" name="email"/>
 
                         <Form.Control.Feedback type="invalid">Please enter a valid email!</Form.Control.Feedback>
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridPassword">
                         <Form.Label>Phone</Form.Label>
-                        <Form.Control required type="text" placeholder="0124567890" />
+                        <Form.Control required type="text" placeholder="0124567890" name="phone"/>
                         <Form.Control.Feedback type="invalid">Please enter your phone number!</Form.Control.Feedback>
 
                     </Form.Group>
@@ -63,14 +99,14 @@ function Register() {
                 <Form.Row>
                     <Form.Group as={Col} controlId="formGridDate">
                         <Form.Label>Select Date to Start</Form.Label>
-                        <Form.Control required type="date" placeholder="01-01-2021" />
+                        <Form.Control required type="date" placeholder="01-01-2021" name="startDate" />
                         <Form.Control.Feedback type="invalid">Please enter the start date</Form.Control.Feedback>
 
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridExpertise">
                         <Form.Label>Expertise</Form.Label>
-                        <Form.Control required as="select" defaultValue="Choose...">
+                        <Form.Control required as="select" defaultValue="Choose..." name="expertise">
                             <option>Photographer</option>
                             <option>Videographer</option>
                         </Form.Control>
